0f1f6f5c51ed87f45f9607ee96fe2ffd367ca4ce
jps <me@jspenc.com>
2020-04-28 23:35:41 +0100

    playing with paket, seems to have working with fsx

--- scrape.fsx
+++ scrape.fsx
@@ -1,1 +1,20 @@
-let one = set ['a', 'b']
+#load @".paket/load/netcoreapp3.1/FSharp.Data.fsx"
+//#r "packages/FSharp.Data/lib/netstandard2.0/FSharp.Data.dll"
+
+open FSharp.Data
+
+let [<Literal>] F1 = 
+  "https://en.wikipedia.org/wiki/" +
+    "2017_FIA_Formula_One_World_Championship"
+
+let f1 = new HtmlProvider<F1>()
+
+
+let something = 
+    f1.Tables.``Season calendar``.Rows
+    |> Seq.sortBy (fun race -> race.Round)
+    |> Seq.take 1
+    
+
+
+//Chart.Column
